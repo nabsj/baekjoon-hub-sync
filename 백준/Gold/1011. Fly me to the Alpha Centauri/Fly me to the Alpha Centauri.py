c2df72905c5d0d445d1b59762f28044f016776d2
bbaekk.98 <62463922+nabsj@users.noreply.github.com>
2024-04-08 01:36:48 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 396 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,18 @@
+T = int(input()) 
+
+for i in range(T):
+    x, y = map(int, input().split())
+
+    d = y - x #거리
+
+    n = 0
+
+    while True:
+        if d <= n*(n+1):
+            break
+        n += 1
+        
+    if d <= n**2:
+        print(n*2-1)
+    else:
+        print(n*2)
