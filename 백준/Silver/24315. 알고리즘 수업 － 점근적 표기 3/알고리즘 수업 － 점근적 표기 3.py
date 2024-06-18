3ebb75e209509ef5c643b1947ddca12bfe9f9ba3
bbaekk.98 <62463922+nabsj@users.noreply.github.com>
2024-06-18 22:54:46 +0900

    [Silver IV] Title: 알고리즘 수업 - 점근적 표기 3, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/24315. 알고리즘 수업 － 점근적 표기 3/알고리즘 수업 － 점근적 표기 3.py
+++ 백준/Silver/24315. 알고리즘 수업 － 점근적 표기 3/알고리즘 수업 － 점근적 표기 3.py
@@ -3,7 +3,7 @@ c1, c2 = map(int, input().split())
 n0 = int(input())
 
 check = True
-for n in range(n0, 1001):
+for n in range(n0, 101):
     if not (c1 * n <= a1 * n + a0 <= c2 * n):
         check = False
         break
